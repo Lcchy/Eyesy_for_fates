06c707f9a22a351c54abb5c666f4bbf43dde64ac
Tim Cosgrove <tim@timcosgrove.net>
2020-11-15 20:39:39 -0800

    Updated deploy file to install all dependencies.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,17 @@
 
 set -ex
 
-# copy files
-sudo rm -f /lib/systemd/system/eyesy-python.service     #In case it is reinstalling
+# Add nodejs Debian package as source.
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+
+# Debian packages
+sudo apt install -y python-pygame python-liblo python-alsaaudio python-pip nodejs
+
+# Python packages
+sudo pip install psutil cherrypy
+
+# Node packages
+cd web/node && npm install && cd ../..
 
 services=("eyesy-python.service" "eyesy-web.service" "eyesy-web-socket.service" "eyesy-pd.service")
 for i in "${services[@]}"
