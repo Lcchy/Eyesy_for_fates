268bd2f1afa2aa6c92fc0614f7d3e1bec81bb2b6
Lcchy <52164716+Lcchy@users.noreply.github.com>
2020-11-28 14:39:47 +0100

    Inclde norns framebuffer fix -> run alternate norns-matron

---
+++ Eyesy_Norns/norns-matron-launcher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+framebuffer="/dev/fb0"
+if [ -e /dev/fb1 ] ; then
+  framebuffer="/dev/fb1"
+fi
+/home/we/norns/build/ws-wrapper/ws-wrapper ws://*:5555 /home/we/norns/build/matron/matron -f${framebuffer}
--- Eyesy_Norns/run.sh
+++ Eyesy_Norns/run.sh
@@ -4,11 +4,15 @@
 touch /tmp/video.log
 
 echo Starting norns
-~/sidekick/patches/Norns/run.sh
+sudo systemctl start norns-jack.service
+sudo systemctl start norns-crone.service
+sudo systemctl start norns-sclang.service
+sudo systemctl start eyesy_norns-matron.service
+sudo systemctl start norns-maiden.service
 sleep 4
 
 echo Starting Eyesy
 systemctl start eyesy_norns-python.service
 systemctl start eyesy-web.service
 systemctl start eyesy-web-socket.service
-# systemctl start eyesy-pd.service # Deactivated to let norns take over the hardware
+# systemctl start eyesy-pd.service          # Deactivated to let norns take over the hardware
